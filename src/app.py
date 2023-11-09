c80e998387f5fd746629bf3c97b0690d2a675cd1
Yoshio KATAOKA <149272540+gasha-research@users.noreply.github.com>
2023-11-09 13:31:49 +0900

    iris_app

--- src/app.py
+++ src/app.py
@@ -1,39 +1,79 @@
 # 必要なモジュールのインポート
+import joblib
 from flask import Flask, request, render_template
-from wtforms import Form, StringField, validators, SubmitField
+from wtforms import Form, FloatField, SubmitField, validators
+import numpy as np
 
-# appという変数名で Flask オブジェクトをインスタンス化
+#　学習済みモデルをもとに推論する関数
+def predict(x):
+    # 学習済みモデル（iris.pkl）を読み込み
+    model = joblib.load('./src/iris.pkl')
+    x = x.reshape(1,-1)
+    pred_label = model.predict(x)
+    return pred_label
+    
+# 推論したラベルから花の名前を返す関数
+def getName(label):
+    if label == 0:
+        return 'Iris Setosa'
+    elif label == 1:
+        return 'Iris Versicolor'
+    elif label == 2:
+        return 'Iris Virginica'
+    else:
+        return 'Error'
+
+# Flaskのインスタンスを作成
 app = Flask(__name__)
 
-# WTForms を使い、index.html 側で表示させるフォームを構築します。
-class InputForm(Form):
-    InputFormTest = StringField('文字を入力してください',
-                    [validators.InputRequired()])
+# 入力フォームの設定
+class IrisForm(Form):
+    SepalLength = FloatField('がくの長さ (0cm ~ 10cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=10, message='0〜10の数値を入力してください')])
 
-    # HTML 側で表示する submit ボタンの表示
-    submit = SubmitField('送信')
+    SepalWidth  = FloatField('がくの幅 (0cm ~ 5cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=5, message='0〜5の数値を入力してください')])
 
+    PetalLength = FloatField('花弁の長さ (0cm ~ 10cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=10, message='0〜10の数値を入力してください')])
+
+    PetalWidth  = FloatField('花弁の幅 (0cm ~ 5cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=5, message='0〜5の数値を入力してください')])
+
+    # html 側で表示する submit ボタンの設定
+    submit = SubmitField('判定')
+    
 # URL にアクセスがあった場合の挙動の設定
 @app.route('/', methods = ['GET', 'POST'])
-def input():
-    # WTForms で構築したフォームをインスタンス化
-    form = InputForm(request.form)
-
-    # POST メソッドの条件の定義
+def predicts():
+    # TForms で構築したフォームをインスタンス化
+    irisForm = IrisForm(request.form)
+    # POST メソッドの定義
     if request.method == 'POST':
 
-        # 条件に当てはまらる場合
-        if form.validate() == False:
-            return render_template('index.html', forms=form)
-        # 条件に当てはまらない場合の処理の実行を定義
+        # 条件に当てはまらない場合
+        if irisForm.validate() == False:
+            return render_template('index.html', forms=irisForm)
+        # 条件に当てはまる場合の、推論を実行
         else:
-            outputname_ = request.form['InputFormTest']
-            return render_template('result.html', outputname=outputname_)
+            VarSepalLength = float(request.form['SepalLength'])
+            VarSepalWidth  = float(request.form['SepalWidth'])
+            VarPetalLength = float(request.form['PetalLength'])
+            VarPetalWidth  = float(request.form['PetalWidth'])
+            # 入力された値を np.array に変換して推論
+            x = np.array([VarSepalLength, VarSepalWidth, VarPetalLength, VarPetalWidth])
+            pred = predict(x)
+            irisName_ = getName(pred)
+            return render_template('result.html', irisName=irisName_)
 
-    # GET メソッドの定義
+    # GET 　メソッドの定義
     elif request.method == 'GET':
-        return render_template('index.html', forms=form)
+        return render_template('index.html', forms=irisForm)
 
-# アプリケーションの実行の定義
+# アプリケーションの実行
 if __name__ == '__main__':
     app.run(debug=True)
